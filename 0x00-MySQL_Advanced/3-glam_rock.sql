3999aff16c36d31bf35364fe51b3590579260a36
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-12 13:06:18 +0300

    Reduce docs

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,4 @@
--- This SQL script select band_name, and lifespan column which is difference of
---  split and formed column, and ordered by lifespan descending from
---  'metal_bands' table, where the Glam Rock is present in the style column.
+-- This SQL script select band_name, and lifespan column which is difference of split and formed column, and ordered by lifespan descending from 'metal_bands' table, where the Glam Rock is present in the style column.
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
